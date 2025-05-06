e4bc02c3a0c4ddaae8dd3c321d0c48182d5165cb
Muthuraman S <smuthuraman204@gmail.com>
2025-05-06 09:34:59 +0530

    100th video.

---
+++ Friends_App_Data/Helpers/Concerns/AppRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends_App_Data.Helpers.Concerns
+{
+    public static class AppRole
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+
+        public static readonly IReadOnlyList<string> All = new[]
+        {
+            Admin,
+            User
+        };
+    }
+}
+ 
--- Friends_App_Data/Helpers/DbInitializer.cs
+++ Friends_App_Data/Helpers/DbInitializer.cs
@@ -5,49 +5,104 @@ using System.Text;
 using System.Threading.Tasks;
 using Friends_App_Data.Data;
 using Friends_App_Data.Data.Models;
+using Friends_App_Data.Helpers.Concerns;
+using Microsoft.AspNetCore.Identity;
 
 namespace Friends_App_Data.Helpers
 {
     public static class DbInitializer
     {
-        public static async Task SeedAsync(AppDbContext appContext)
+        public static async Task SeedUsersAsync(UserManager<User> userManager, RoleManager<IdentityRole<int>> roleManager)
         {
-            if (!appContext.Users.Any() && !appContext.Posts.Any())
+            //Roles
+            if (!roleManager.Roles.Any())
             {
+                foreach (var role in AppRole.All)
+                {
+                    if (!await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole<int>(role));
+                    }
+                }
+            }
+
+            //Users with Roles
+            if (!userManager.Users.Any(n => !string.IsNullOrEmpty(n.Email)))
+            {
+                var userPassword = "Muthuraman@1234";
+
                 var newUser = new User()
                 {
+                    UserName = "muthuraman",
+                    Email = "smuthuram47@gmail.com",
                     FullName = "Muthuraman S",
-                    ProfilePictureUrl = "~/images/profilePictures/Muthuraman.jpg"
+                    ProfilePictureUrl = "~/images/profilePictures/Muthuraman.jpg",
+                    EmailConfirmed = true
                 };
 
-                await appContext.Users.AddAsync(newUser);
-                await appContext.SaveChangesAsync();
+                var result = await userManager.CreateAsync(newUser, userPassword);
 
-                var newPostWithUser = new Post()
+                if (result.Succeeded)
                 {
-                    Content = "This is going to be our first post which is being loaded from the database." +
-                    "and it has been created using our text user.",
-                    ImageUrl = "",
-                    NoOfReports = 0,
-                    DateCreated = DateTime.UtcNow,
-                    DateUpdated = DateTime.UtcNow,
-                    UserId = newUser.Id,
-                };
+                    await userManager.AddToRoleAsync(newUser, AppRole.User);
+                }
 
-                var newPostWithImage = new Post()
+                var newAdmin = new User()
                 {
-                    Content = "This is going to be our second post which is being loaded from the database." +
-                    "and it has been created using our text user.",
-                    ImageUrl = "https://th.bing.com/th/id/OIP.N8F8QgLuxGEUhZeovsyjJwHaFN?rs=1&pid=ImgDetMain",
-                    NoOfReports = 0,
-                    DateCreated = DateTime.UtcNow,
-                    DateUpdated = DateTime.UtcNow,
-                    UserId = newUser.Id,
+                    UserName = "admin.admin",
+                    Email = "admin.friends@gmail.com",
+                    FullName = "Muthuraman Admin",
+                    ProfilePictureUrl = "~/images/profilePictures/Muthuraman.jpg",
+                    EmailConfirmed = true
                 };
 
-                await appContext.Posts.AddRangeAsync(newPostWithImage, newPostWithUser);
-                await appContext.SaveChangesAsync();
+                var resultAdmin = await userManager.CreateAsync(newAdmin, userPassword);
+
+                if (resultAdmin.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(newUser, AppRole.Admin);
+                }
             }
+        }
+
+        public static async Task SeedAsync(AppDbContext appContext)
+        {
+            //if (!appContext.Users.Any() && !appContext.Posts.Any())
+            //{
+            //    var newUser = new User()
+            //    {
+            //        FullName = "Muthuraman S",
+            //        ProfilePictureUrl = "~/images/profilePictures/Muthuraman.jpg"
+            //    };
+
+            //    await appContext.Users.AddAsync(newUser);
+            //    await appContext.SaveChangesAsync();
+
+            //    var newPostWithUser = new Post()
+            //    {
+            //        Content = "This is going to be our first post which is being loaded from the database." +
+            //        "and it has been created using our text user.",
+            //        ImageUrl = "",
+            //        NoOfReports = 0,
+            //        DateCreated = DateTime.UtcNow,
+            //        DateUpdated = DateTime.UtcNow,
+            //        UserId = newUser.Id,
+            //    };
+
+            //    var newPostWithImage = new Post()
+            //    {
+            //        Content = "This is going to be our second post which is being loaded from the database." +
+            //        "and it has been created using our text user.",
+            //        ImageUrl = "https://th.bing.com/th/id/OIP.N8F8QgLuxGEUhZeovsyjJwHaFN?rs=1&pid=ImgDetMain",
+            //        NoOfReports = 0,
+            //        DateCreated = DateTime.UtcNow,
+            //        DateUpdated = DateTime.UtcNow,
+            //        UserId = newUser.Id,
+            //    };
+
+            //    await appContext.Posts.AddRangeAsync(newPostWithImage, newPostWithUser);
+            //    await appContext.SaveChangesAsync();
+            //}
         }
     }
 }
