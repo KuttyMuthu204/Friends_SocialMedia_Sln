1922500806ecb7895cebe431089aa4d29de43f7f
Muthuraman S <smuthuraman204@gmail.com>
2025-05-07 10:56:22 +0530

    115th video.

--- Friends_SocialMedia_UI/Controllers/SettingsController.cs
+++ Friends_SocialMedia_UI/Controllers/SettingsController.cs
@@ -1,7 +1,10 @@
-﻿using Friends_App_Data.Helpers.Enums;
+﻿using System.Security.Claims;
+using Friends_App_Data.Data.Models;
+using Friends_App_Data.Helpers.Enums;
 using Friends_App_Data.Services;
 using Friends_SocialMedia_UI.ViewModels.Settings;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Friends_SocialMedia_UI.Controllers
@@ -11,18 +14,21 @@ namespace Friends_SocialMedia_UI.Controllers
     {
         private readonly IUsersService _usersService;
         private readonly IFilesService _filesService;
+        private readonly UserManager<User> _userManager;
 
-        public SettingsController(IUsersService usersService, IFilesService filesService)
+        public SettingsController(IUsersService usersService, IFilesService filesService, UserManager<User> userManager)
         {
             _usersService = usersService;
             _filesService = filesService;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
         {
-            var loggedInUserId = 1;
-            var user = await _usersService.GetUser(loggedInUserId);
-            return View(user);
+            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _usersService.GetUser(int.Parse(loggedInUserId));
+            var loggedInUser = await _userManager.GetUserAsync(User);
+            return View(loggedInUser);
         }
 
         [HttpPost]
