d14ac7df95a22083e2f5b293c769cd93e6eb1da5
Muthuraman S <smuthuraman204@gmail.com>
2025-05-10 01:06:26 -0700

    131th video.

---
+++ Friends_App_Data/Helpers/Constants/FriendShipStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friends_Data.Helpers.Constants
+{
+    public static class FriendShipStatus
+    {
+        public const string Pending = "Pending";
+        public const string Accepted = "Accepted";
+        public const string Rejected = "Rejected";
+        public const string Blocked = "Blocked";
+        public const string Unblocked = "Unblocked";
+        public const string Cancelled = "Cancelled";
+    }
+}
---
+++ Friends_App_Data/Services/FriendsService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Friends_App_Data.Data;
+using Friends_Data.Data.Models;
+using Friends_Data.Helpers.Constants;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Friends_Data.Services
+{
+    public class FriendsService : IFriendsService
+    {
+        private readonly AppDbContext _context;
+
+        public FriendsService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SendRequest(int senderId, int receiverId)
+        {
+            var request = new FriendRequest()
+            {
+                SenderId = senderId,
+                ReceiverId = receiverId,
+                Status = FriendShipStatus.Pending,
+                DateCreated = DateTime.UtcNow,
+                DateUpdated = DateTime.UtcNow
+            };
+
+            await _context.FriendRequests.AddAsync(request);    
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
