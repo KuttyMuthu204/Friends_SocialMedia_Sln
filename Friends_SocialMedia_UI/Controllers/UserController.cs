41ad4256ef1235b396d917f5b94e5549d8ed4335
Muthuraman S <smuthuraman204@gmail.com>
2025-05-09 20:20:07 -0700

    123th video.

---
+++ Friends_SocialMedia_UI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Friends_SocialMedia_UI.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Friends_UI.Controllers
+{
+    public class UserController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(string userId)
+        {
+            return View();
+        }
+    }
+}
