9f65c9bd9a77841d0b70bd49079faef5fe92eca6
Muthuraman S <smuthuraman204@gmail.com>
2025-05-10 00:09:47 -0700

    128th video.

--- Friends_SocialMedia_UI/Controllers/UserController.cs
+++ Friends_SocialMedia_UI/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Friends_App_Data.Services;
+﻿using Friends_App_Data.Data.Models;
+using Friends_App_Data.Services;
 using Friends_SocialMedia_UI.Controllers.Base;
+using Friends_UI.ViewModels.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Friends_UI.Controllers
@@ -7,10 +10,12 @@ namespace Friends_UI.Controllers
     public class UserController : BaseController
     {
         private readonly IUsersService _usersService;
+        private readonly UserManager<User> _userManager;
 
-        public UserController(IUsersService usersService)
+        public UserController(IUsersService usersService, UserManager<User> userManager)
         {
             _usersService = usersService;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -21,7 +26,15 @@ namespace Friends_UI.Controllers
         public async Task<IActionResult> Details(int userId)
         {
             var allPosts = await _usersService.GetUserPosts(userId);
-            return View(allPosts);
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+
+            var userProfileVM = new GetUserProfileVM()
+            {
+                User = user,
+                Posts = allPosts
+            };
+
+            return View(userProfileVM);
         }
     }
 }
---
+++ Friends_SocialMedia_UI/ViewModels/User/GetUserProfileVM.cs
@@ -0,0 +1,10 @@
+﻿using Friends_App_Data.Data.Models;
+
+namespace Friends_UI.ViewModels.User
+{
+    public class GetUserProfileVM
+    {
+        public List<Post> Posts { get; set; }
+        public Friends_App_Data.Data.Models.User User { get; set; }
+    }
+}
